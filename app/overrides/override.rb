70f5ea6c76cac93f46f893e6ecf45ad5166d20f8
lrjbrual <lrjbrual@live.com>
2016-09-06 17:52:27 +0200

    Adding jumbotron

--- app/overrides/override.rb
+++ app/overrides/override.rb
@@ -1,5 +1,22 @@
-# Deface::Override.new(
-#   virtual_path: "spree/shared/_sidebar",
-#   name: "remove_sidebar",
-#   remove: "aside#sidebar"
-#   )
+Deface::Override.new(
+  virtual_path: "spree/shared/_sidebar",
+  name: "remove_sidebar",
+  remove: "aside#sidebar"
+  )
+
+
+
+Deface::Override.new(
+ virtual_path: "spree/home/index",
+ name: "login",
+ insert_before: "[data-hook='homepage_products']",
+ text: "<div class='jumbotron'>
+        <div class='container text-center'>
+        <div id='jumbotron-spacing'>
+        </div>
+        <p>
+          <a class='btn btn-lg btn-success'  role='button'><%= link_to Spree.t(:shop_now), spree.products_path %></a>
+        </p>
+        </div>
+      </div>
+      ")
