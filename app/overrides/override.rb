75580ce804645a52b79da1150451c1c61a9dd3b7
lrjbrual <lrjbrual@live.com>
2016-09-06 23:52:18 +0200

    Adding layout design

--- app/overrides/override.rb
+++ app/overrides/override.rb
@@ -4,11 +4,11 @@ Deface::Override.new(
   remove: "aside#sidebar"
   )
 
-
+##for Jumbotron
 Deface::Override.new(
- virtual_path: "spree/home/index",
- name: "login",
- insert_before: "[data-hook='homepage_products']",
+ virtual_path: "spree/layouts/spree_application",
+ name: "jumbotron",
+ insert_before: "div#content",
  text: "<div class='jumbotron'>
         <div class='container text-center'>
         <div id='jumbotron-spacing'>
@@ -18,4 +18,27 @@ Deface::Override.new(
         </p>
         </div>
       </div>
-      ")
+      "
+)
+
+#deface content
+  Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
+  name: "remove_sidebar",
+  replace: "div#content") do
+    "
+    <div id='content' class='<%= !content_for?(:sidebar) ? 'col-sm-12' : 'col-sm-8 col-md-12' %>' data-hook>
+      <%= flash_messages %>
+      <%= yield %>
+    </div>
+    "
+  end
+
+#deface body for java script
+Deface::Override.new(
+ virtual_path: "spree/layouts/spree_application",
+ name: "icon-return",
+ insert_before: "div#spree-header",
+ text: "
+ <a href='javascript:' id='return-to-top'><i class='icon-chevron-up'></i></a>
+ "
+ )
