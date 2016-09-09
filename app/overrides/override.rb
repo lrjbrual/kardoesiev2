95b5eef3f3f1f3deec0176b5f32ba804bcd58835
lrjbrual <lrjbrual@live.com>
2016-09-09 15:43:21 +0200

    Adding overide jumbotron

--- app/overrides/override.rb
+++ app/overrides/override.rb
@@ -6,9 +6,9 @@ Deface::Override.new(
 
 ##for Jumbotron
 Deface::Override.new(
- virtual_path: "spree/layouts/spree_application",
+ virtual_path: "spree/home/index",
  name: "jumbotron",
- insert_before: "div#content",
+ insert_before: "[data-hook='homepage_products']",
  text: "<div class='jumbotron'>
         <div class='container text-center'>
         <div id='jumbotron-spacing'>
@@ -24,21 +24,28 @@ Deface::Override.new(
 #deface content
   Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
   name: "remove_sidebar",
-  replace: "div#content") do
+  replace: "div.container") do
     "
-    <div id='content' class='<%= !content_for?(:sidebar) ? 'col-sm-12' : 'col-sm-8 col-md-12' %>' data-hook>
-      <%= flash_messages %>
-      <%= yield %>
-    </div>
+      <div class='container'>
+         <div class='row' data-hook>
+           <%= breadcrumbs(@taxon) %>
+           <%= render partial: 'spree/shared/sidebar' if content_for? :sidebar %>
+          <div id='content' class='<%= !content_for?(:sidebar) ? 'col-sm-12' : 'col-sm-8 col-md-12' %>' data-hook>
+            <%= flash_messages %>
+            <%= yield %>
+          </div>
+          <%= yield :templates %>
+        </div>
+      </div>
     "
   end
 
-#deface body for java script
-Deface::Override.new(
- virtual_path: "spree/layouts/spree_application",
- name: "icon-return",
- insert_before: "div#spree-header",
- text: "
- <a href='javascript:' id='return-to-top'><i class='icon-chevron-up'></i></a>
- "
- )
+# #deface body for java script
+# Deface::Override.new(
+#  virtual_path: "spree/layouts/spree_application",
+#  name: "icon-return",
+#  insert_before: "div#spree-header",
+#  text: "
+#  <a href='javascript:' id='return-to-top'><i class='icon-chevron-up'></i></a>
+#  "
+#  )
