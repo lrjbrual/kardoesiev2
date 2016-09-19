3c8a75aca87bdd1db5cdb67bfc099ca1a39f709e
lrjbrual <lrjbrual@live.com>
2016-09-20 00:47:42 +0200

    changing color

--- app/overrides/override.rb
+++ app/overrides/override.rb
@@ -14,10 +14,26 @@ Deface::Override.new(
         <div id='jumbotron-spacing'>
         </div>
           <%= link_to Spree.t(:shop_now), spree.products_path, class: 'btn-shop' %>
+          <div class='fb-like' data-href='https://www.facebook.com/Kardoesie-Vivace-and-Cotton-Road-590068331165047/'' data-layout='button_count' data-action='like' data-size='large' data-show-faces='false' data-share='true'></div>
         </div>
       </div>
       "
 )
+# Deface::Override.new(
+#  virtual_path: "spree/home/index",
+#  name: "facebook",
+#  insert_before: "div.container") do
+#    "
+#        <div id='fb-root'></div>
+#       <script>(function(d, s, id) {
+#       var js, fjs = d.getElementsByTagName(s)[0];
+#       if (d.getElementById(id)) return;
+#       js = d.createElement(s); js.id = id;
+#       js.src = ''//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.7&appId=1288595431165026';
+#       fjs.parentNode.insertBefore(js, fjs);
+#       }(document, 'script', 'facebook-jssdk'));</script>
+#       "
+#     end
 
 #deface content
   Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
